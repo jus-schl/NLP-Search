97bbeb57f3991e7e9e557350923fb0fd92b043e9
Mullil <eetu.taatila@gmail.com>
2025-01-29 11:31:03 +0200

    issue #7 remove redundant logic

--- week2/boolean-search.py
+++ week2/boolean-search.py
@@ -20,23 +20,14 @@ td_matrix = dense_matrix.T
 
 t2i = cv.vocabulary_
 
-
-previous_token = None
-def rewrite_token(t, next_token=None):
-    global previous_token
-    if t == previous_token and previous_token not in d: # if the token was already handled an empty string is returned
-        previous_token = None
-        return ''
+def rewrite_token(t):
     if t not in d and t not in t2i: # handle unknown tokens
         return '(0)'
-    if t in ["NOT", "not"] and next_token not in d and next_token not in t2i: # if the operator is NOT and the next token is unknown (1) is returned
-        previous_token = next_token
-        return '(1)'
     return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t))
 
 def rewrite_query(query):
     tokens = query.split()
-    return " ".join(rewrite_token(tokens[i], tokens[i + 1] if i + 1 < len(tokens) else None) for i in range(len(tokens))) # if possible, the next token is also given to the function
+    return " ".join(rewrite_token(token) for token in tokens) # if possible, the next token is also given to the function
 
 
 #main loop
