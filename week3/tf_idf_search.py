d6ddb6ab9e001424c651408ccec82b48fab137ff
jus-schl <justus.schlueter@gmx.de>
2025-02-05 18:17:19 +0100

    issue #11: tf-idf-search + directly implementable in queries.py

---
+++ week3/tf_idf_search.py
@@ -0,0 +1,45 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+
+d = {"and": "&", "AND": "&",
+     "or": "|", "OR": "|",
+     "not": "1 -", "NOT": "1 -",
+     "(": "(", ")": ")"}
+
+
+def rewrite_token(t):
+    if t not in d and t not in t2i: # handle unknown tokens
+        return 'np.matrix([0] * sparse_td_matrix.shape[1])'
+    return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t))
+
+def rewrite_query(query):
+    tokens = query.split()
+    return " ".join(rewrite_token(token) for token in tokens) # if possible, the next token is also given to the function
+
+
+def return_docs(input_query, documents):
+    tfv4 = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
+    sparse_td_matrix = tfv4.fit_transform(documents).T.tocsr()
+    t2i = tfv4.vocabulary_
+  
+    hits_matrix = eval(rewrite_query(input_query))
+    hits_matrix = np.asarray(hits_matrix).flatten()
+    hits_list = hits_matrix.nonzero()[0]
+          
+  
+    sorted_results = sorted(
+        [(doc_idx, hits_matrix[doc_idx]) for doc_idx in hits_list],
+        key=lambda x: x[1],
+        reverse=True
+    )
+    for doc_idx, score in sorted_results:
+        print(f"Matching doc #{doc_idx} (Score: {score:.4f}): {documents[doc_idx]}")
+
+
+    
+
+
+
+        # 1h work on rewriting 
+        # 2h work on being dumb
