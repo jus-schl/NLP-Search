f1da9e319c26bf66a2e6c7dec498265c9d04cad9
Mullil <eetu.taatila@gmail.com>
2025-02-28 17:39:20 +0200

    issue #39 add error messages and prevent crashing

--- routes.py
+++ routes.py
@@ -1,6 +1,5 @@
 from app import app
 from flask import render_template, request, redirect, session
-import traceback
 import query
 import songs
 import urllib.parse
@@ -8,29 +7,29 @@ import urllib.parse
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "GET":
-        return render_template('index.html')
-    try:
-        if request.method == "POST":
-            user_input = request.form.get('query')
-            engine = int(request.form.get('engine'))
-            session['engine'] = engine
-            encoded_input = urllib.parse.quote(user_input)
-            return redirect(f'/process/{encoded_input}')
-    except Exception as e:
-        print("Error in /:", e)
-        print(traceback.format_exc())
+        engine = session.get("engine", 3)
+        return render_template('index.html', engine = session['engine'])
+    if request.method == "POST":
+        user_input = request.form.get('query')
+        engine = int(request.form.get('engine'))
+        session['engine'] = engine
+        if not user_input:
+            return render_template('index.html', engine = engine)
+        encoded_input = urllib.parse.quote(user_input)
+        return redirect(f'/process/{encoded_input}')
+
 
 @app.route('/process/<encoded_input>', methods=['GET', 'POST'])
 def process(encoded_input):
-    try:
-        user_input = urllib.parse.unquote(encoded_input)
-        if request.method == "GET":
-            engine = session['engine']
-            results = query.search_songs(user_input, engine)
-            return render_template('index.html', results = results)
-    except Exception as e:
-        print("Error in /process:", e)
-        print(traceback.format_exc())
+    user_input = urllib.parse.unquote(encoded_input)
+    if request.method == "GET":
+        engine = session['engine']
+        results = query.search_songs(user_input, engine)
+        if results:
+            return render_template('index.html', results = results, engine = engine)
+        else:
+            error_message = "Query failed"
+            return render_template('index.html', error_message = error_message, engine = engine)
 
 @app.route('/songs/<song_id>', methods=['GET', 'POST'])
 def song(song_id):
